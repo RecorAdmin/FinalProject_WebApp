6954385442e61aaa0fde86fa5f356ee8f84e2c63
Recordo Barclay <rbarclay19j@vtdi.edu.jm>
2023-04-26 12:16:29 -0500

    Add project files.

---
+++ WebApplication_EvotesNet_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_EvotesNet_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
